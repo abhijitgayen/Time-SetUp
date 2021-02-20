c0c59d465288221e0db25071c99d51dd18018412
Abhijit Gayen <76001287+zero26-hack-bit@users.noreply.github.com>
2021-02-20 14:45:09 +0530

    Update background.js

--- Time SetUp/scripts/background.js
+++ Time SetUp/scripts/background.js
@@ -26,7 +26,7 @@ function updateStorage() {
 }
 
 function backgroundCheck() {
-    chrome.windows.getLastFocused({ populate: true }, function(currentWindow) {
+    chrome.windows.getLastFocused({ populate: true }, function (currentWindow) {
         if (currentWindow.focused) {
             var activeTab = currentWindow.tabs.find(t => t.active === true);
             if (activeTab !== undefined && activity.isValidPage(activeTab)) {
@@ -47,7 +47,7 @@ function backgroundCheck() {
                         if (currentTab !== tab.url) {
                             activity.setCurrentActiveTab(tab.url);
                         }
-                        chrome.idle.queryState(parseInt(setting_interval_inactivity), function(state) {
+                        chrome.idle.queryState(parseInt(setting_interval_inactivity), function (state) {
                             if (state === 'active') {
                                 mainTRacker(activeUrl, tab, activeTab);
                             }/* else checkDOM(state, activeUrl, tab, activeTab);*/
@@ -91,21 +91,21 @@ function mainTRacker(activeUrl, tab, activeTab) {
 }
 
 function showNotification(activeUrl, tab) {
-    chrome.notifications.clear('watt-site-notification', function(wasCleared) {
+    chrome.notifications.clear('watt-site-notification', function (wasCleared) {
         if (!wasCleared) {
             console.log('!wasCleared');
 
             chrome.notifications.create(
                 'watt-site-notification', {
-                    type: 'basic',
-                    iconUrl: 'icons/clock128px.png',
-                    title: "Time SetUp Says",
-                    contextMessage: activeUrl + ' ' + convertShortSummaryTimeToString(tab.getTodayTime()),
-                    message: setting_notification_message
-                },
-                function(notificationId) {
+                type: 'basic',
+                iconUrl: 'icons/clock128px.png',
+                title: "Time SetUp Says",
+                contextMessage: activeUrl + ' ' + convertShortSummaryTimeToString(tab.getTodayTime()),
+                message: setting_notification_message
+            },
+                function (notificationId) {
                     console.log(notificationId);
-                    chrome.notifications.clear('watt-site-notification', function(wasCleared) {
+                    chrome.notifications.clear('watt-site-notification', function (wasCleared) {
                         if (wasCleared)
                             notificationAction(activeUrl, tab);
                     });
@@ -119,17 +119,17 @@ function showNotification(activeUrl, tab) {
 function notificationAction(activeUrl, tab) {
     chrome.notifications.create(
         'watt-site-notification', {
-            type: 'basic',
-            iconUrl: 'icons/clock128px.png',
-            title: "Time SetUp Says",
-            contextMessage: activeUrl + ' ' + convertShortSummaryTimeToString(tab.getTodayTime()),
-            message: setting_notification_message
-        });
+        type: 'basic',
+        iconUrl: 'icons/clock128px.png',
+        title: "Time SetUp Says",
+        contextMessage: activeUrl + ' ' + convertShortSummaryTimeToString(tab.getTodayTime()),
+        message: setting_notification_message
+    });
 }
 
 function setBlockPageToCurrent(activeUrl) {
     var blockUrl = chrome.runtime.getURL("control.html") + '?url=' + activeUrl;
-    chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
         chrome.tabs.update(tab.id, { url: blockUrl });
     });
 }
@@ -151,7 +151,7 @@ function setDefaultSettings() {
 }
 
 function checkSettingsImEmpty() {
-    chrome.storage.local.getBytesInUse(['inactivity_interval'], function(item) {
+    chrome.storage.local.getBytesInUse(['inactivity_interval'], function (item) {
         if (item == 0) {
             setDefaultSettings();
         }
@@ -163,18 +163,18 @@ function setDefaultValueForNewSettings() {
 }
 
 function addListener() {
-    chrome.tabs.onActivated.addListener(function(info) {
-        chrome.tabs.get(info.tabId, function(tab) {
+    chrome.tabs.onActivated.addListener(function (info) {
+        chrome.tabs.get(info.tabId, function (tab) {
             activity.addTab(tab);
         });
     });
 
-    chrome.webNavigation.onCompleted.addListener(function(details) {
-        chrome.tabs.get(details.tabId, function(tab) {
+    chrome.webNavigation.onCompleted.addListener(function (details) {
+        chrome.tabs.get(details.tabId, function (tab) {
             activity.updateFavicon(tab);
         });
     });
-    chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.runtime.onInstalled.addListener(function (details) {
         if (details.reason == 'install') {
             storage.saveValue(SETTINGS_SHOW_HINT, SETTINGS_SHOW_HINT_DEFAULT);
             setDefaultSettings();
@@ -186,7 +186,7 @@ function addListener() {
             isNeedDeleteTimeIntervalFromTabs = true;
         }
     });
-    chrome.storage.onChanged.addListener(function(changes, namespace) {
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
         for (var key in changes) {
             if (key === STORAGE_BLACK_LIST) {
                 loadignorlist();
@@ -201,19 +201,19 @@ function addListener() {
                 loadNotificationMessage();
             }
             if (key === SETTINGS_INTERVAL_INACTIVITY) {
-                storage.getValue(SETTINGS_INTERVAL_INACTIVITY, function(item) { setting_interval_inactivity = item; });
+                storage.getValue(SETTINGS_INTERVAL_INACTIVITY, function (item) { setting_interval_inactivity = item; });
             }
             if (key === SETTINGS_VIEW_TIME_IN_BADGE) {
-                storage.getValue(SETTINGS_VIEW_TIME_IN_BADGE, function(item) { setting_view_in_badge = item; });
+                storage.getValue(SETTINGS_VIEW_TIME_IN_BADGE, function (item) { setting_view_in_badge = item; });
             }
         }
     });
 
-     chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSflgDcrH-LKfolR8Gq2lJq7s3Q-tXzkN1Gg2GJFh__KeqJJDQ/viewform");
+    chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSflgDcrH-LKfolR8Gq2lJq7s3Q-tXzkN1Gg2GJFh__KeqJJDQ/viewform");
 }
 
 function loadTabs() {
-    storage.loadTabs(STORAGE_TABS, function(items) {
+    storage.loadTabs(STORAGE_TABS, function (items) {
         tabs = [];
         if (items != undefined) {
             for (var i = 0; i < items.length; i++) {
@@ -226,8 +226,8 @@ function loadTabs() {
 }
 
 function deleteTimeIntervalFromTabs() {
-    tabs.forEach(function(item) {
-        item.days.forEach(function(day) {
+    tabs.forEach(function (item) {
+        item.days.forEach(function (day) {
             if (day.time != undefined)
                 day.time = [];
         })
@@ -239,13 +239,13 @@ function deleteYesterdayTimeInterval() {
 }
 
 function loadignorlist() {
-    storage.getValue(STORAGE_BLACK_LIST, function(items) {
+    storage.getValue(STORAGE_BLACK_LIST, function (items) {
         setting_black_list = items;
     })
 }
 
 function loadTimeIntervals() {
-    storage.getValue(STORAGE_TIMEINTERVAL_LIST, function(items) {
+    storage.getValue(STORAGE_TIMEINTERVAL_LIST, function (items) {
         timeIntervalList = [];
         if (items != undefined) {
             for (var i = 0; i < items.length; i++) {
@@ -257,19 +257,19 @@ function loadTimeIntervals() {
 }
 
 function loadRestrictionList() {
-    storage.getValue(STORAGE_RESTRICTION_LIST, function(items) {
+    storage.getValue(STORAGE_RESTRICTION_LIST, function (items) {
         setting_restriction_list = items;
     })
 }
 
 function loadNotificationList() {
-    storage.getValue(STORAGE_NOTIFICATION_LIST, function(items) {
+    storage.getValue(STORAGE_NOTIFICATION_LIST, function (items) {
         setting_notification_list = items;
     });
 }
 
 function loadNotificationMessage() {
-    storage.getValue(STORAGE_NOTIFICATION_MESSAGE, function(item) {
+    storage.getValue(STORAGE_NOTIFICATION_MESSAGE, function (item) {
         setting_notification_message = item;
         if (isEmpty(setting_notification_message)) {
             storage.saveValue(STORAGE_NOTIFICATION_MESSAGE, STORAGE_NOTIFICATION_MESSAGE_DEFAULT);
@@ -279,8 +279,8 @@ function loadNotificationMessage() {
 }
 
 function loadSettings() {
-    storage.getValue(SETTINGS_INTERVAL_INACTIVITY, function(item) { setting_interval_inactivity = item; });
-    storage.getValue(SETTINGS_VIEW_TIME_IN_BADGE, function(item) { setting_view_in_badge = item; });
+    storage.getValue(SETTINGS_INTERVAL_INACTIVITY, function (item) { setting_interval_inactivity = item; });
+    storage.getValue(SETTINGS_VIEW_TIME_IN_BADGE, function (item) { setting_view_in_badge = item; });
 }
 
 function loadAddDataFromStorage() {
@@ -292,34 +292,34 @@ function loadAddDataFromStorage() {
     loadNotificationMessage();
     loadSettings();
 }
- 
+
 function checkPermissionsForNotifications(callback, ...props) {
     chrome.permissions.contains({
         permissions: ["notifications"]
-    }, function(result) {
+    }, function (result) {
         if (callback != undefined && result)
             callback(...props);
         isHasPermissioForNotification = result;
     });
 }
 // rest notification
-var alarm = chrome.alarms.create("myAlarm", { delayInMinutes: 10.0, periodInMinutes:10.0});
-chrome.alarms.onAlarm.addListener( function (alarm) {
-  chrome.notifications.clear('take-a-break-notification');
-  var notification = chrome.notifications.create(
-    'take-a-break-notification', {
-      type: 'basic',
-      iconUrl: "icons/clock32px.png", 
-      title: 'Time SetUP Says',
-      message: 'Take a break! Look away from your computer screen and focus on a spot 20 feet away for 50 seconds.'
+var alarm = chrome.alarms.create("myAlarm", { delayInMinutes: 10.0, periodInMinutes: 10.0 });
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    chrome.notifications.clear('take-a-break-notification');
+    var notification = chrome.notifications.create(
+        'take-a-break-notification', {
+        type: 'basic',
+        iconUrl: "icons/clock32px.png",
+        title: 'Time SetUP Says',
+        message: 'Take a break! Look away from your computer screen and focus on a spot 20 feet away for 50 seconds.'
     },
-  );
- });
+    );
+});
 // var alarm2= chrome.alarms.create("myAlarm2", { delayInMinutes: 200.0, periodInMinutes:10000.0 });
 
 // chrome.alarms.onAlarm.addListener( function(alarm2) {
 //   chrome.notifications.clear('take-a-break-notification2');
-  
+
 //   var notification2 = chrome.notifications.create(
 //     'take-a-break-notification2', {
 //       type: 'basic',
